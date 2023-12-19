897974d0b34fb932635bbdfa22a1fee42273a193
lambda <ejgdlyz@163.com>
2023-12-19 17:01:15 +0800

    socket 封装：Address 模块实现与测试

---
+++ sylar/address.h
@@ -0,0 +1,143 @@
+#ifndef __SYLAR_ADDRESS_H__
+#define __SYLAR_ADDRESS_H__
+
+#include <memory>
+#include <string>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <iostream>
+#include <arpa/inet.h>
+#include <sys/un.h>
+#include <map>
+#include <vector>
+
+namespace sylar {
+
+class IPAddress;
+
+class Address {
+public:
+    typedef std::shared_ptr<Address> ptr;
+    ~Address() {}
+    
+    static Address::ptr Create(const sockaddr* addr, socklen_t);
+    static bool Lookup(std::vector<Address::ptr>& result, const std::string& host, 
+            int family = AF_UNSPEC, int type = 0, int protocol = 0);
+   
+    static Address::ptr LookupAny(const std::string& host,
+                        int family = AF_UNSPEC, int type = 0, int protocol = 0);
+
+    static std::shared_ptr<IPAddress> LookupAnyIPAddress(const std::string& host,
+                        int family = AF_UNSPEC, int type = 0, int protocol = 0);
+
+    // 通过网卡得到 IPAddress
+    static bool GetInterfaceAddresses(std::multimap<std::string, std::pair<Address::ptr, uint32_t>>& res, 
+                    int family = AF_UNSPEC);
+    static bool GetInterfaceAddresses(std::vector<std::pair<Address::ptr, uint32_t>>& res, 
+                    const std::string& iface, int family = AF_UNSPEC);
+
+    int getFamily() const;                                      // socket 类型
+
+    virtual const sockaddr* getAddr() const = 0;
+    virtual socklen_t getAddrLen() const = 0;
+
+    virtual std::ostream& insert(std::ostream& os) const = 0;   // 序列化 
+    std::string toString();
+
+    bool operator<(const Address& rhs) const;
+    bool operator==(const Address& rhs) const;
+    bool operator!=(const Address& rhs) const;
+};
+
+class IPAddress : public Address {
+public:
+    typedef std::shared_ptr<IPAddress> ptr;
+
+    static IPAddress::ptr Create(const char* address, uint32_t  port = 0); // IPv4/IPv6 域名转 IP地址
+
+    virtual IPAddress::ptr broadcastAddress(uint32_t prefix_len) = 0;   // 通过 IP 得到广播地址
+    virtual IPAddress::ptr networkAddress(uint32_t prefix_len) = 0;     // 网络地址
+    virtual IPAddress::ptr subnetMask(uint32_t prefix_len) = 0;         // 子网掩码
+
+    virtual uint32_t getPort() const = 0;
+    virtual void setPort(uint32_t v) = 0;
+};
+
+class IPv4Address : public IPAddress {
+public:
+    typedef std::shared_ptr<IPv4Address> ptr;
+
+    static IPv4Address::ptr Create(const char* address, uint32_t port = 0);             // str -> sockaddr_in
+
+    IPv4Address(const sockaddr_in& address);
+    IPv4Address(uint32_t address = INADDR_ANY, uint32_t port = 0);
+    
+    const sockaddr* getAddr() const override;
+    socklen_t getAddrLen() const override;
+    std::ostream& insert(std::ostream& os) const override;           // 序列化 
+    
+    IPAddress::ptr broadcastAddress(uint32_t prefix_len) override;   // 通过 IP 得到广播地址
+    IPAddress::ptr networkAddress(uint32_t prefix_len) override;     // 网络地址
+    IPAddress::ptr subnetMask(uint32_t prefix_len) override;         // 子网掩码
+
+    uint32_t getPort() const override;
+    void setPort(uint32_t v) override;
+
+private:
+    sockaddr_in m_addr;
+};
+
+class IPv6Address : public IPAddress {
+public:
+    typedef std::shared_ptr<IPv6Address> ptr;
+    IPv6Address();
+    IPv6Address(const sockaddr_in6 & address);
+    IPv6Address(const uint8_t address[16], uint32_t port = 0);
+
+    static IPv6Address::ptr Create(const char* address, uint32_t port = 0);
+
+    const sockaddr* getAddr() const override;
+    socklen_t getAddrLen() const override;
+    std::ostream& insert(std::ostream& os) const override;           // 序列化 
+    
+    IPAddress::ptr broadcastAddress(uint32_t prefix_len) override;   // 通过 IP 得到广播地址
+    IPAddress::ptr networkAddress(uint32_t prefix_len) override;     // 网络地址
+    IPAddress::ptr subnetMask(uint32_t prefix_len) override;         // 子网掩码
+
+    uint32_t getPort() const override;
+    void setPort(uint32_t v) override;
+
+private:
+    sockaddr_in6 m_addr;
+};
+
+class UnixAddress : public Address {
+public:
+    typedef std::shared_ptr<UnixAddress> ptr;
+    UnixAddress();
+    UnixAddress(const std::string& path);
+
+    const sockaddr* getAddr() const override;
+    socklen_t getAddrLen() const override;
+    std::ostream& insert(std::ostream& os) const override;   // 序列化 
+
+private:
+    struct sockaddr_un m_addr;
+    socklen_t m_length;
+};
+
+class UnknownAddress : public Address {
+public:
+    typedef std::shared_ptr<UnixAddress> ptr;
+    UnknownAddress(int family);
+    UnknownAddress(const sockaddr& addr);
+    const sockaddr* getAddr() const override;
+    socklen_t getAddrLen() const override;
+    std::ostream& insert(std::ostream& os) const override;   // 序列化 
+private:
+    sockaddr m_addr;
+};
+
+}
+
+#endif  // __SYLAR_ADDRESS_H__
---
+++ tests/test_address.cc
@@ -0,0 +1,71 @@
+#include "sylar/address.h"
+#include "sylar/log.h"
+
+sylar::Logger::ptr g_logger = SYLAR_LOG_ROOT();
+
+void test_address() {
+    std::vector<sylar::Address::ptr> addrs;
+    // bool v = sylar::Address::Lookup(addrs, "www.baidu.com");
+    bool v = sylar::Address::Lookup(addrs, "www.baidu.com:80");
+    // bool v = sylar::Address::Lookup(addrs, "www.baidu.com:http");  // http -> 80
+    // bool v = sylar::Address::Lookup(addrs, "www.baidu.com:ftp");  // ftp -> 21
+
+
+    if (!v) {
+        SYLAR_LOG_ERROR(g_logger) << "lookup fail";
+        return;
+    }
+
+    for (size_t i = 0; i < addrs.size(); ++i) {
+        SYLAR_LOG_INFO(g_logger) << i << " - " << addrs[i]->toString();
+    }
+}
+
+void test_iface() {
+    std::multimap<std::string, std::pair<sylar::Address::ptr, uint32_t>> results;
+    bool v = sylar::Address::GetInterfaceAddresses(results);
+    if (!v) {
+        SYLAR_LOG_ERROR(g_logger) << "GetInterfaceAddresses fail";
+        return;
+    }
+
+    for (auto& m: results) {
+        SYLAR_LOG_INFO(g_logger) << m.first << " - " << m.second.first->toString() << " - " << m.second.second;
+    }
+}
+
+void test_ipv4() {
+    // auto addr = sylar::IPAddress::Create("www.baidu.com");
+    auto addr = sylar::IPAddress::Create("127.0.0.8");
+
+    if (addr) {
+         SYLAR_LOG_INFO(g_logger) << addr->toString();
+    }
+}
+
+void test_networkaddress() {
+    const char* ip = "192.168.233.128";
+    const int port = 80;
+    sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_port = htons(port);
+    int result = inet_pton(AF_INET, ip, &addr.sin_addr);
+    if (result <= 0) {
+        SYLAR_LOG_INFO(g_logger) << "inet_pton error";
+    }
+    sylar::IPv4Address::ptr ipv4(new sylar::IPv4Address(addr));
+    int prefix_len = 24;
+    SYLAR_LOG_INFO(g_logger) << "ipv4 address: " << ipv4->toString();
+    SYLAR_LOG_INFO(g_logger) << "broadcast address: " << ipv4->broadcastAddress(prefix_len)->toString();
+    SYLAR_LOG_INFO(g_logger) << "network address: " << ipv4->networkAddress(prefix_len)->toString();
+    SYLAR_LOG_INFO(g_logger) << "subnet mask: " << ipv4->subnetMask(prefix_len)->toString();
+
+}
+
+int main() {
+    // test();
+    // test_iface();
+    // test_ipv4();
+    test_networkaddress();
+    return 0;
+}
